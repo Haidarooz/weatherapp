61d55d72a320e749c8ce54de005543e880db7804
Haidar Al-Ogaily <Haidar@wemakeapps.net>
2021-11-29 08:47:12 +1100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/haidars_weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.haidars_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const kLinearGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Colors.purple, Colors.blue],
+);
+
+Icon kCloudIcon = Icon(
+  FontAwesomeIcons.cloud,
+  size: 75.0,
+  color: Colors.white,
+);
+
+Icon kSunIcon = Icon(
+  FontAwesomeIcons.sun,
+  size: 75.0,
+  color: Colors.white,
+);
+
+Icon kMoonIcon = Icon(
+  FontAwesomeIcons.moon,
+  size: 75.0,
+  color: Colors.white,
+);
+
+String apiKey = "e5b9cf379cb561c51b5edc3b304396f5";
